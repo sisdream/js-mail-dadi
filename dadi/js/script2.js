c8f5f3768e38b55d068ca5635cc677a8e7450562
sisdream <sissipascale2@gmail.com>
2024-01-12 15:16:53 +0100

    feat: dadi

---
+++ dadi/js/script2.js
@@ -0,0 +1,16 @@
+// generatore numeri random
+
+let dado = Math.floor(Math.random() * 6 + 1);
+console.log(dado);
+
+// generatore del computer
+
+let pcDado = Math.floor(Math.random() * 6 + 1);
+console.log(pcDado);
+
+
+
+
+
+
+
