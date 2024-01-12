836b84a133b7bd6526be6127dccfe05f42c46c08
sisdream <sissipascale2@gmail.com>
2024-01-12 15:39:18 +0100

    feat:  evento

--- dadi/js/script2.js
+++ dadi/js/script2.js
@@ -8,9 +8,21 @@ console.log(dado);
 let pcDado = Math.floor(Math.random() * 6 + 1);
 console.log(pcDado);
 
+//  variabile append
 
+const result = document.getElementById('result');
 
+// evento
 
+document.getElementById('play').addEventListener('click',
 
-
-
+    function(){
+        if(dado === pcDado){
+            result.innerText = "avete pareggiato!";
+        }else if (dado > pcDado){
+            result.innerText = "ha vinto l'utente!";
+        }else{
+            result.innerText = "ha vinto il pc!";
+        }
+    }
+)
