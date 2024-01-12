4edfbbf3792f5449f70b55562d46f86ecd424d7f
sisdream <sissipascale2@gmail.com>
2024-01-12 15:03:08 +0100

    feat: ciclo for

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ console.log("Queste sono le mail: " + mailList);
 
 // input
 let mail = prompt("Qual'è la tua email?");
+
+
+// convalidare
+const valid = document.getElementById('true');
+const invalid = document.getElementById('false')
+
+
+// ciclo for
+
+let x = false;
+
+for(let i = 0; i < mailList.length; i++){
+  if(mail === mailList[i]){
+    console.log(`${mail} trovata al posto: ${i}`)
+    x = true;
+  }
+}
+
+if(x === true){
+  valid.append("La tua mail c'è!");
+}else{
+  valid.append("Mi dispiace, la tua mail non c'è.");
+}
